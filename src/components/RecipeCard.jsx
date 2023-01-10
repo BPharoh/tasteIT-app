95e5dab1e6e713aca8be5212318761d24d0357df
Otulugbu Alexander <s2200742@Alexander-MacBook-Air.local>
2023-01-10 17:08:17 +0200

    Making recipe card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect} from 'react';
 import { Link } from "react-router-dom";
 
 const RecipeCard = () => {
+
+        useEffect(() => {
+        axios.get("http://localhost:3001/recipes")
+        .then((response) => {
+            let allRecipes = [];
+            response.data.map((item) => { 
+                allRecipes.push(item);
+                return(allRecipes)     
+    })
+    console.log('allRecipes', allRecipes);
+});
+}, []);
+
     return (
         <div className='recipe-card'>
             <img src="https://plus.unsplash.com/premium_photo-1663127648893-390677703b5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG1hc2hlZCUyMHBvdGF0b3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60" alt="mashed-potatoes"/>
--- src/pages/RecipesPlus.jsx
+++ src/pages/RecipesPlus.jsx
@@ -1,45 +1,153 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 const RecipesPlus = () => {
+
+const [ countryList, setCountryList ] = useState([]);
+// const [ instructions, setInstructions ] = useState([]);
+const [ingred, setIngred] = useState([{id: 1, item:'', quantity:''}])
+// const [isLoading, setIsLoading] = useState(false);
+const [ uploadRecipe, setUploadRecipe ] = useState({
+        name: '',
+        author: '',
+        country: '',
+        description: '',
+        link: '',
+        ingredients: [],
+        instructions: []
+        });
+// const [ showpopup, setShowpopup ] = useState(false);
+      
+      
+const inputHandler = (e) => {
+      setUploadRecipe({...uploadRecipe,  [e.target.name]:e.target.value})
+      console.log('recipe:', uploadRecipe)
+    };
+
+const ingredientHandler = (e, index) => {
+    const {name, value} = e.target;
+    const addMore = [...ingred];
+    addMore[index][name] = value;
+    setIngred(addMore);
+    setUploadRecipe({ ...uploadRecipe, ingredients: ingred });
+      };
+
+// const instructionsHandler =(e)=>{
+//         const name = e.target.name;
+//         let value = e.target.value;
+//         value = value.split('\n').map(str => <span>{str}<br/></span>);
+//         setUploadRecipe({ ...uploadRecipe, [name]: value });
+//         }
+
+const addMoreInputsHandler = (e) => {
+        e.preventDefault();
+        setIngred([...ingred,{id: ingred.length + 1,  item: '', quantity: '' }]);
+    };
+
+const postHandler = (e) => {
+    axios.post("http://localhost:3001/recipes", uploadRecipe)
+    console.log('recipe:', uploadRecipe)
+    }
+    
+const submitHandler = (e) => {
+      e.preventDefault();
+      postHandler(uploadRecipe)
+      window.location.reload()
+    };
+
+
+
+useEffect(() => {
+// setIsLoading(true)
+axios.get('https://restcountries.com/v3.1/all/')
+.then((response) => {
+let countries = [];
+response.data.map((item) => { 
+    countries.push(item.name.common);
+    countries.sort();
+    return(countries)
+})
+setCountryList(countries);
+console.log('RestAPI:', countries);
+});
+}, []);
+
+// if(isLoading){
+//     return <p>Loading...</p>
+//   }
+
+
+
+
     return (
         <div>
             <h1>Add new recipe</h1>
-            <form action="" method="post">
+            <form action="" method="post" onChange={inputHandler} onSubmit={submitHandler} {...uploadRecipe}>
                 <div className="form-group">
-                    <label htmlFor="recipe-name">Name</label>
-                    <input type="text" name="recipe-name" />
+                    <label htmlFor="name">Name:
+                    <input 
+                    type="text" 
+                    name="name" 
+                    onChange={inputHandler}
+                    /></label>
                 </div>
                 <div className="form-group">
                     <label htmlFor="author">Author</label>
-                    <input type="text" name="author" />
+                    <input 
+                    type="text" 
+                    name="author"
+                    onChange={inputHandler}
+                     />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="recipe-source">Recipe is from:</label>
-                    <input type="text" name="recipe-source" />
-                </div>
+                <label htmlFor="countries">Recipe Source</label>
+                <select name="country" onChange={inputHandler}>
+                <option selected="selected">Select a country</option>
+                {countryList.map((index) => {
+            return ( <option value={index} key={index}>{index}</option>);
+            })}</select>
+            </div>
                 <div className="form-group">
                     <label htmlFor="description">Description</label>
-                    <textarea name="description" id="description" cols="30" rows="10"></textarea>
+                    <textarea 
+                    name="description" 
+                    onChange={inputHandler}
+                    />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="recipe-url">Image</label>
-                    <input type="text" name="recipe-url" />
+                    <label htmlFor="link">Image</label>
+                    <input 
+                    type="text" 
+                    name="link" 
+                    onChange={inputHandler}
+                    />
                 </div>
-                <div className="form-group">
-                    <label htmlFor="ingredients">Ingredients</label>
+                <p>Ingredients</p>
+                {ingred.map((_, index) => {
+                    return (
+                    <div className="form-group" key={index}>
                     <div className="add-more">
-                    <label htmlFor="ingredients">Quantity</label>
-                    <input type="text" name="ingredients" />
-                    <label htmlFor="ingredients">Ingredient</label>
-                    <input type="text" name="ingredients" />
+                    <label htmlFor="item">Item</label>
+                    <input type="text" name="item" id="item" onChange={(e) => ingredientHandler(e, index)}/>
                     </div>
-                    <button type='button'>add more</button>
-                </div>
-                <div className="form-group">
+                    <div className="add-more">
+                    <label htmlFor="quantity">Quantity</label>
+                    <input type="text" name="quantity" id="quantity" onChange={(e) => ingredientHandler(e, index)}/>
+                    </div>
+                    </div> 
+                    );
+                })}
+                <button type='button' onClick={addMoreInputsHandler}>add more</button>
+                
+  
+                <div className="form-group instructions">
                     <label htmlFor="instructions">Instructions</label>
-                    <textarea name="instructions" id="instructions" cols="30" rows="10"></textarea>
+                    <textarea 
+                    name="instructions" 
+                    />
+
                 </div>
-                <button type="submit">Post recipe</button>
+                <button type="submit" >Post recipe</button>
             </form>
         </div>
     );
